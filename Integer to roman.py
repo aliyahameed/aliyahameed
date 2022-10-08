0ac365a16cfed33ed8228a6d124967ceb2e9c0a9
aliyahameed <114465416+aliyahameed@users.noreply.github.com>
2022-10-08 05:42:05 +0100

    Add files via upload

---
+++ Integer to roman.py
@@ -0,0 +1,16 @@
+num=int(input("Enter the number :"))
+m = ["", "M", "MM", "MMM"]
+c = ["", "C", "CC", "CCC", "CD", "D",
+         "DC", "DCC", "DCCC", "CM "]
+x = ["", "X", "XX", "XXX", "XL", "L",
+         "LX", "LXX", "LXXX", "XC"]
+i = ["", "I", "II", "III", "IV", "V",
+         "VI", "VII", "VIII", "IX"]
+thousands = m[num // 1000]
+hundreds = c[(num % 1000) // 100]
+tens = x[(num % 100) // 10]
+ones = i[num % 10]
+result = (thousands + hundreds +tens + ones)
+print(result)
+  
+  
